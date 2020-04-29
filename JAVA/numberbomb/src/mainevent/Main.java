44d27e2694aaf6d4029d0de81562dc09af62850d
RKOC <30385661+rkoCyrus@users.noreply.github.com>
2020-04-30 00:50:10 +0800

    Add jar, package metadata and better kill task process

--- JAVA/numberbomb/src/mainevent/Main.java
+++ JAVA/numberbomb/src/mainevent/Main.java
@@ -1,6 +1,7 @@
 package mainevent;
 
 import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 /**
@@ -15,52 +16,56 @@ public class Main {
     /**
      * Main event of the program
      */
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, NoSuchElementException {
         //Play another round or exit
         boolean isAgain = false;
 
         //Player input numbers
         int inputNumber;
 
-        //Full process
-        do {
-            pg = new PreGame();
-            ig = new InGame(pg.getGameRequire());
-
-            //Game procecss
+        try {
+            // Full process
             do {
-                try {
-                    key = new Scanner(System.in);
-                    ig.getCurrentRange();
-                    System.out.print("Please enter the number what you guest: ");
-                    inputNumber = key.nextInt();
-                    ig.inputLoad(inputNumber);
-                } catch (InputMismatchException e) {
-                    //When user typed non integer
-                    System.out.println("Please enter number, please\n");
-                }
-            } while (!ig.isPoped()); //Until someone guest right
+                pg = new PreGame();
+                ig = new InGame(pg.getGameRequire());
 
-            //Print out lose message
-            System.out.println("You lose! The program picked " + pg.getTheNumber() + "!");
+                // Game procecss
+                do {
+                    try {
+                        key = new Scanner(System.in);
+                        ig.getCurrentRange();
+                        System.out.print("Please enter the number what you guest: ");
+                        inputNumber = key.nextInt();
+                        ig.inputLoad(inputNumber);
+                    } catch (InputMismatchException e) {
+                        // When user typed non integer
+                        System.out.println("Please enter number, please\n");
+                    }
+                } while (!ig.isPoped()); // Until someone guest right
 
-            //Consider is play again or exit
-            boolean illegalInput = true;
-            while (illegalInput) {
-                System.out.print("Play again? (Y/N)");
-                char yn = key.next().charAt(0);
-                switch (yn) {
-                    case 'Y':
-                    case 'y':
-                        isAgain = true;
-                    case 'N':
-                    case 'n':
-                        illegalInput = false;
-                        break;
-                    default:
-                        System.out.println("Sorry, please type again");
+                // Print out lose message
+                System.out.println("You lose! The program picked " + pg.getTheNumber() + "!");
+
+                // Consider is play again or exit
+                boolean illegalInput = true;
+                while (illegalInput) {
+                    System.out.print("Play again? (Y/N)");
+                    char yn = key.next().charAt(0);
+                    switch (yn) {
+                        case 'Y':
+                        case 'y':
+                            isAgain = true;
+                        case 'N':
+                        case 'n':
+                            illegalInput = false;
+                            break;
+                        default:
+                            System.out.println("Sorry, please type again");
+                    }
                 }
-            }
-        } while (isAgain); //Consider need play another round
+            } while (isAgain); // Consider need play another round
+        } catch (NoSuchElementException e) {
+            System.exit(0);
+        }
     }
 }
