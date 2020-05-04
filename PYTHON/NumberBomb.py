c9fe44219efb4fdffb7e0e5e994a7b8ab4f52b35
RKOC <30385661+rkoCyrus@users.noreply.github.com>
2020-05-04 23:18:22 +0800

    Upload python file

---
+++ PYTHON/NumberBomb.py
@@ -0,0 +1,77 @@
+# I make this as one file only
+# I have no idea how Python work since there is no requirment of class which make me confuse on parsing data
+# To ensure it work and upload to pip, it's better to use one file for entire program
+
+#Import
+import random
+import time
+
+# Global variable 
+minRange = 1
+maxRange = 0
+luckNum = 0
+userInput = -1
+
+
+def compareNumber(user,computer):
+    """Compare does user input 'Lucky' number
+
+    Args:
+        user (int): user input
+        computer (int): computer generated number
+
+    Returns:
+        booleans
+    """
+    if int(user)==int(computer):
+        return True
+    
+    return False
+
+
+while True:
+    try:
+        diff = input("Difficulties? (1:1-100, 2:1-250, 3:1-500) : ")
+        if (int(diff) >= 1 and int(diff) <= 3):
+            swithcer = {
+                1:100,
+                2:250,
+                3:500
+            }
+            maxRange = swithcer.get(int(diff))
+            break
+        else:
+            print("Please select 1-3")
+            continue
+    except ValueError:
+        print("Number please!")
+        continue
+
+# When finished while loop, pick the number
+luckNum = random.randint(2,maxRange-1)
+    
+while True:
+    try:
+        print("\nCurrent range : " + str(minRange) + " - " + str(maxRange) + '\n')
+        kbInput = input("Please pick the number : ")
+        time.sleep(3.0)
+        if (int(kbInput) > minRange and int(kbInput) < maxRange):
+            if (compareNumber(int(kbInput),luckNum)):
+                break
+            else:
+                if int(kbInput) > luckNum:
+                    maxRange = int(kbInput)
+                else:
+                    minRange = int(kbInput)
+            continue
+        else:
+            print('\n'+"Invalid input!!!"+'\n')
+            continue
+    except ValueError:
+        print("\nCome on, input integer please...\n")
+        continue
+
+print("\nThe balloon poped!\nThe \"lucky number\" is " + str(luckNum) + "\n\nYou lose!\n")
+exit(0)
+
+
