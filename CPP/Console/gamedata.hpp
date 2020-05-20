05765937f3e53cf99c0d7a8a43bcb3952968982a
RKOC <30385661+rkoCyrus@users.noreply.github.com>
2020-05-20 21:38:46 +0800

    Upload better coding files

---
+++ CPP/Console/gamedata.hpp
@@ -0,0 +1,64 @@
+#ifndef GAMEDATA_H
+#define GAMEDATA_H
+#define RAND_MAX = 65535
+#include <random>
+#include <time.h>
+namespace NumberBomb
+{
+    class Gamedata
+    {
+    private:
+        //Pick a number for loading "Lucky Number"
+        int luckNum;
+        //Previous lucky number
+        int preLuckNum;
+        //Check does user decide to play this game or not
+        bool isPlay;
+        //Generate lucky number
+        void genLuck()
+        {
+            do
+            {
+                srand(time(NULL));
+                luckNum = rand()%maxNum;
+            }
+            while (luckNum==preLuckNum||luckNum<=1);
+        }
+    protected:
+        //Maximum number in a range of number
+        int maxNum;
+        //Get lucky number for ingame (via pregame)
+        int getLuck()
+        {
+            genLuck();
+            return luckNum;
+        }
+    public:
+        Gamedata();
+        Gamedata(int);
+        bool playing()
+        {
+            return isPlay;
+        }
+        void endgame()
+        {
+            isPlay = false;
+        }
+    };
+    
+    //Initial game
+    Gamedata::Gamedata()
+    {
+        isPlay = true;
+        this->preLuckNum = 0;
+    }
+
+    //Next round of the game
+    Gamedata::Gamedata(int preLuckNum)
+    {
+        isPlay = true;
+        this->preLuckNum = preLuckNum;
+    }
+    
+}
+#endif
---
+++ CPP/Console/pregame.hpp
@@ -0,0 +1,26 @@
+#ifndef PREGAME_H
+#define PREGAME_H
+#include "gamedata.hpp"
+namespace NumberBomb
+{
+    class Pregame : public Gamedata
+    {
+    private:
+
+    public:
+        Pregame();
+        Pregame(int);
+    };
+    
+    Pregame::Pregame()
+    {
+
+    }
+
+    Pregame::Pregame(int preLuck)
+    {
+
+    }
+    
+}
+#endif
