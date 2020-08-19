faef571581758262c6bc0873e03f587ce4872a10
RKOC <30385661+rkoCyrus@users.noreply.github.com>
2020-08-19 16:12:59 +0800

    Depoly lucky number function

--- C/GameValue.h
+++ C/GameValue.h
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <time.h>
 #ifndef GAME_VALUE
 #define GAME_VALUE
 
@@ -8,3 +10,19 @@ struct GameValue
 };
 
 #endif
+
+//Generate luckyNumber
+int genLuckNum(int max)
+{
+    srand(time(0));
+    int result = 0;
+    do
+    {
+        result = rand();
+        if (result>=max)
+        {
+            result %= max;
+        }
+    } while (result<=1);
+    return result;
+}
